dd1d6b8e02b7c2553b3eca4d955e127a7c4ec76b
Kustav <you@example.com>
2023-03-31 00:15:54 +0530

    chat css v2

--- chat.php
+++ chat.php
@@ -4,98 +4,105 @@
 
 <style>
   .wrapper {
-  display: flex;
-  justify-content: center;
-  align-items: center;
-  height: 100vh;
-}
+    display: flex;
+    justify-content: center;
+    align-items: center;
+    height: 100vh;
+  }
 
-.users {
-  width: 500px;
-  border: 1px solid #ccc;
-  border-radius: 5px;
-  overflow: hidden;
-}
+  .users {
+    width: 500px;
+    border: 1px solid #ccc;
+    border-radius: 5px;
+    overflow: hidden;
+  }
 
-.users header {
-  display: flex;
-  align-items: center;
-  padding: 10px;
-  background-color: #f1f1f1;
-}
+  .users header {
+    display: flex;
+    align-items: center;
+    padding: 0;
+    background-color: #f1f1f1;
+    height: 50px;
+  }
 
-.users header .content {
-  display: flex;
-  flex-direction: column;
-  margin-left: 10px;
-}
+  .users header .content {
+    display: flex;
+    flex-direction: column;
+    margin-left: 10px;
+    flex: 1; /* add this line */
+  }
 
-.users header .content span {
-  font-size: 18px;
-  font-weight: bold;
-}
+  .users header .content span {
+    font-size: 18px;
+    font-weight: bold;
+  }
 
-.users header .content p {
-  font-size: 14px;
-  color: #666;
-  margin: 0;
-}
+  .users header .content p {
+    font-size: 14px;
+    color: #666;
+    margin: 0;
+  }
 
-.users header .back-icon {
-  color: #666;
-  text-decoration: none;
-  margin-right: 10px;
-}
+  .users header .back-icon {
+    color: #666;
+    text-decoration: none;
+    margin-right: 10px;
+  }
 
-.users header .back-icon:hover {
-  color: #000;
-}
+  .users header .back-icon:hover {
+    color: #000;
+  }
 
-.chat-box {
-  height: 300px;
-  overflow-y: scroll;
-  padding: 10px;
-}
+  .chat-box {
+    height: 600px !important;
+    overflow-y: scroll;
+    flex: 1; /* add this line */
+    padding: 0; /* change from 10px to 0 */
+  }
 
-.typing-area {
-  display: flex;
-  align-items: center;
-  padding: 10px;
-  background-color: #f1f1f1;
-}
+  .typing-area {
+    display: flex;
+    align-items: center;
+    padding: 0; /* change from 10px to 0 */
+    background-color: #f1f1f1;
+  }
 
-.typing-area .input-field {
-  flex: 1;
-  margin-right: 10px;
-  padding: 5px;
-  border: none;
-  border-radius: 20px;
-  outline: none;
-}
+  .typing-area .input-field {
+    flex: 1;
+    margin-right: 10px;
+    padding: 5px;
+    border: none;
+    border-radius: 20px;
+    outline: none;
+  }
 
-.typing-area button {
-  background-color: #0084ff;
-  color: #fff;
-  border: none;
-  border-radius: 50%;
-  width: 40px;
-  height: 40px;
-  display: flex;
-  justify-content: center;
-  align-items: center;
-  cursor: pointer;
-}
+  .typing-area button {
+    background-color: #0084ff;
+    color: #fff;
+    border: none;
+    border-radius: 50%;
+    width: 40px;
+    height: 40px;
+    display: flex;
+    justify-content: center;
+    align-items: center;
+    cursor: pointer;
+  }
 
-.typing-area button:hover {
-  background-color: #0073e6;
-}
+  .typing-area button:hover {
+    background-color: #0073e6;
+  }
+
 </style>
+<head>
+<link href="https://use.fontawesome.com/releases/v6.1.0/css/all.css" rel="stylesheet">
+</head>
 <body>
 <div class="wrapper">
     <section class="users">
       <header>
         <div class="content">
-      <header>
+      
         <?php 
           $user_id = mysqli_real_escape_string($link, $_GET['user_id']);
           $sql = mysqli_query($link, "SELECT * FROM users WHERE id = {$user_id}");
@@ -118,12 +125,11 @@
       <form action="#" class="typing-area">
         <input type="text" class="incoming_id" name="incoming_id" value="<?php echo $user_id; ?>" hidden>
         <input type="text" name="message" class="input-field" placeholder="Type a message here..." autocomplete="off">
-        <button><i class="fab fa-telegram-plane"></i></button>
+        <button><i class="fa fa-paper-plane"></i></button>
       </form>
     </section>
   </div>
-  </section>
-  </div>
+
   <!-- <script src="chat/chat.js"></script> -->
 
 </body>
